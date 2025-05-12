180a356e2ea101f7bfd70677323b62a678fd693b
Trono41 <trono1441@gmail.com>
2025-05-11 18:14:35 -0700

    updated ArcaneBolt.SetProperties()

--- Assets/Scripts/Spells/ArcaneBolt.cs
+++ Assets/Scripts/Spells/ArcaneBolt.cs
@@ -12,9 +12,24 @@ public class ArcaneBolt : Spell
 
     override public void SetProperties(JObject properties)
     {
-        this.name = properties["name"].ToString();
-        UnityEngine.Debug.Log(this.name);
-        return;
+        name = properties["name"].ToString();
+        icon = properties["icon"].ToObject<int>();
+        description = properties["description"].ToObject<string>();
+        damage = properties["damage"]["amount"].ToString();
+        damage_type = Damage.TypeFromString(properties["damage"]["type"].ToString());
+        mana_cost = properties["mana_cost"].ToString();
+        cooldown = properties["cooldown"].ToString();
+        projectile["trajectory"] = properties["projectile"]["trajectory"].ToString();
+        projectile["speed"] = properties["projectile"]["speed"].ToString();
+        projectile["sprite"] = properties["projectile"]["sprite"].ToString();
+        Debug.Log(name);
+        Debug.Log(icon);
+        Debug.Log(description);
+        Debug.Log(damage);
+        Debug.Log(damage_type);
+        Debug.Log(mana_cost);
+        Debug.Log(cooldown);
+        Debug.Log(projectile);
     }
 
 
